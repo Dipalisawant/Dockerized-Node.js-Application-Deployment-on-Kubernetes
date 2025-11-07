73673c9445ed052b38bb8acd9d5aa13480090236
Dipali <155541661+Dipalisawant@users.noreply.github.com>
2025-11-07 11:57:00 +0530

    Fix typo in response message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Docker and Node.js!');
+  res.send('Helloo from Docker and Node.js!');
 });
 
 app.listen(PORT, () => {
